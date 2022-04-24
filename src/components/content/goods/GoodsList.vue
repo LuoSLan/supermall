db4e12407781843d252bae93f898be7726f4841a
LuoSLan <1550527769@qq.com>
2022-04-25 01:11:04 +0800

    20220425

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="item in goods" :key="item" :goodsItem="item" />
+    <!-- {{ goods }} -->
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem.vue";
+export default {
+  components: { GoodsListItem },
+  name: "GoodsList",
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,47 +6,92 @@
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
     <feature-view></feature-view>
-    <tab-control :titles="['流行', '新款', '精选']" class="tab-control" />
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <tab-control
+      :titles="['流行', '新款', '精选']"
+      class="tab-control"
+      @tabClick="tabClick"
+    />
+    <goods-list :goods="goods[currentType].list" />
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
 import TabControl from "components/content/tabControl/TabControl.vue";
+import GoodsList from "components/content/goods/GoodsList.vue";
 
 import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendView from "./childComps/RecommendView.vue";
 import FeatureView from "./childComps/FeatureView.vue";
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar,
     TabControl,
+    GoodsList,
     HomeSwiper,
     RecommendView,
     FeatureView,
+    GoodsList,
     TabControl,
   },
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
     };
   },
   created() {
-    getHomeMultidata().then((res) => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-      // console.log(res);
-    });
+    this.getHomeMultidata();
+
+    //请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    /*
+     *事件监听相关方法
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+      }
+    },
+
+    /*
+     *网络请求相关方法
+     */
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+        // console.log(res);
+      });
+    },
+    getHomeGoods(type) {
+      let page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
   },
 };
 </script>
